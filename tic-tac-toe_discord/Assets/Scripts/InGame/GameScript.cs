ebba788abbf749e81e67795dbe211b3b6191c36c
takeru-a-wada <takeru.a.wada@dena.com>
2024-06-24 11:12:22 +0900

    片方のターンにしかならないバグ

--- tic-tac-toe_discord/Assets/Scripts/InGame/GameScript.cs
+++ tic-tac-toe_discord/Assets/Scripts/InGame/GameScript.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
-using System.Linq;
 public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
 {
     public List<Button> buttonList;
@@ -24,26 +25,8 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
     private string user2Name = "";
     void Start()
     {
-        if (PhotonNetwork.PlayerList.Length == 2)
-        {
-            var player1 = PhotonNetwork.PlayerList[0];
-            var player2 = PhotonNetwork.PlayerList[1];
-            user1Name = player1.NickName;
-            user2Name = player2.NickName;
-            if (PhotonNetwork.IsMasterClient)
-            {
-                currentPlayer = 1;
-                isMyTurn = true;
-                turnDisplay.text = $"{user1Name}のターン";
-            }
-            else
-            {
-                currentPlayer = 2;
-                isMyTurn = false;
-                turnDisplay.text = $"{user2Name}のターン";
-            }
-        }
-
+        // Initial setup for the players
+        SetUpPlayers();
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
@@ -59,6 +42,28 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
         }
         UpdateTurnCountDisplay();
     }
+    private void SetUpPlayers()
+    {
+        if (PhotonNetwork.PlayerList.Length == 2)
+        {
+            var player1 = PhotonNetwork.PlayerList[0];
+            var player2 = PhotonNetwork.PlayerList[1];
+            user1Name = player1.NickName;
+            user2Name = player2.NickName;
+            if (PhotonNetwork.IsMasterClient)
+            {
+                currentPlayer = Random.Range(1, 3);
+                photonView.RPC("SyncInitialPlayer", RpcTarget.AllBuffered, currentPlayer);
+            }
+            UpdateTurnStatus();
+        }
+    }
+    [PunRPC]
+    private void SyncInitialPlayer(int player)
+    {
+        currentPlayer = player;
+        UpdateTurnStatus();
+    }
     private void OnSquareClicked(int x, int y)
     {
         if (!isMyTurn) return;
@@ -70,6 +75,8 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
         else if (previousButtonX == x && previousButtonY == y)
         {
             photonView.RPC("OnSquareConfirmed", RpcTarget.All, x, y, currentPlayer);
+            previousButtonX = -1;
+            previousButtonY = -1;
         }
         else
         {
@@ -80,60 +87,46 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
     [PunRPC]
     private void OnSquareConfirmed(int x, int y, int player)
     {
-        if (squareStatus[x, y] == 0)
+        if (squareStatus[x, y] != 0) return;
+        squareStatus[x, y] = player;
+        buttons[x, y].image.color = (player == 1) ? Color.blue : Color.green;
+        AddPlayerSquare(player, x, y);
+        var winningButtons = CheckWinCondition(player);
+        if (winningButtons.Count > 0)
         {
-            squareStatus[x, y] = player;
-            buttons[x, y].image.color = (player == 1) ? Color.blue : Color.green;
-            if (player == 1)
-            {
-                user1Squares.Add(buttons[x, y].gameObject);
-                if (user1Squares.Count > 3)
-                {
-                    StopBlinking(user1Squares[0].GetComponent<Button>(), Color.blue); // 既存の点滅を停止
-                    GameObject first = user1Squares[0];
-                    string[] coordinates = first.name.Split('-')[1].ToCharArray().Select(c => c.ToString()).ToArray();
-                    squareStatus[int.Parse(coordinates[0]), int.Parse(coordinates[1])] = 0;
-                    first.GetComponent<Button>().image.color = Color.white;
-                    user1Squares.RemoveAt(0);
-                }
-                if (user1Squares.Count == 3 && player == 1)
-                {
-                    BlinkButton(user1Squares[0].GetComponent<Button>(), Color.blue); // 新しい古いボタンを点滅
-                }
-            }
-            else
-            {
-                user2Squares.Add(buttons[x, y].gameObject);
-                if (user2Squares.Count > 3)
-                {
-                    StopBlinking(user2Squares[0].GetComponent<Button>(), Color.green); // 既存の点滅を停止
-                    GameObject first = user2Squares[0];
-                    string[] coordinates = first.name.Split('-')[1].ToCharArray().Select(c => c.ToString()).ToArray();
-                    squareStatus[int.Parse(coordinates[0]), int.Parse(coordinates[1])] = 0;
-                    first.GetComponent<Button>().image.color = Color.white;
-                    user2Squares.RemoveAt(0);
-                }
-                if (user2Squares.Count == 3 && player == 2)
-                {
-                    BlinkButton(user2Squares[0].GetComponent<Button>(), Color.green); // 新しい古いボタンを点滅
-                }
-            }
-            var winningButtons = CheckWinCondition();
-            if (winningButtons.Count > 0)
-            {
-                turnDisplay.text = $"ユーザー{(player == 1 ? user1Name : user2Name)}の勝利";
-                StopAllBlinking();
-                HighlightWinningButtons(winningButtons);
-                DisableAllButtons();
-                StartCoroutine(ReturnToStartMenu());
-            }
-            else
-            {
-                ChangeTurn();
-            }
-            previousButtonX = -1;
-            previousButtonY = -1;
+            turnDisplay.text = $"{(player == 1 ? user1Name : user2Name)}の勝利";
+            StopAllBlinking();
+            HighlightWinningButtons(winningButtons);
+            DisableAllButtons();
+            StartCoroutine(ReturnToStartMenu());
         }
+        else
+        {
+            photonView.RPC("ChangeTurn", RpcTarget.All);
+        }
+    }
+    private void AddPlayerSquare(int player, int x, int y)
+    {
+        List<GameObject> playerSquares = (player == 1) ? user1Squares : user2Squares;
+        Color playerColor = (player == 1) ? Color.blue : Color.green;
+        playerSquares.Add(buttons[x, y].gameObject);
+        if (playerSquares.Count > 3)
+        {
+            RemoveOldestSquare(playerSquares, playerColor);
+        }
+        if (playerSquares.Count == 3)
+        {
+            BlinkButton(playerSquares[0].GetComponent<Button>(), playerColor);
+        }
+    }
+    private void RemoveOldestSquare(List<GameObject> squares, Color playerColor)
+    {
+        StopBlinking(squares[0].GetComponent<Button>(), playerColor);
+        GameObject first = squares[0];
+        string[] coordinates = first.name.Split('-')[1].ToCharArray().Select(c => c.ToString()).ToArray();
+        squareStatus[int.Parse(coordinates[0]), int.Parse(coordinates[1])] = 0;
+        first.GetComponent<Button>().image.color = Color.white;
+        squares.RemoveAt(0);
     }
     private IEnumerator ReturnToStartMenu()
     {
@@ -141,7 +134,6 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
         PhotonNetwork.LeaveRoom();
         PhotonNetwork.LoadLevel("StartMenuScene");
     }
-
     private void UpdateTurnDisplay()
     {
         string turnText = (currentPlayer == 1) ? $"{user1Name}のターン" : $"{user2Name}のターン";
@@ -151,13 +143,23 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
     {
         turnCountDisplay.text = $"{turnCount}ターン目";
     }
+    [PunRPC]
     private void ChangeTurn()
     {
         turnCount++;
         currentPlayer = (currentPlayer == 1) ? 2 : 1;
-        isMyTurn = (currentPlayer == PhotonNetwork.LocalPlayer.ActorNumber);
+        UpdateTurnStatus();
+    }
+    private void UpdateTurnStatus()
+    {
+        bool isMasterClientLocalPlayer = PhotonNetwork.LocalPlayer.IsMasterClient;
+        isMyTurn = (isMasterClientLocalPlayer && currentPlayer == 1) || (!isMasterClientLocalPlayer && currentPlayer == 2);
         UpdateTurnDisplay();
         UpdateTurnCountDisplay();
+        HandleBlinking();
+    }
+    private void HandleBlinking()
+    {
         if (currentPlayer == 1 && user2Squares.Count == 3)
         {
             StopBlinking(user2Squares[0].GetComponent<Button>(), Color.green);
@@ -175,19 +177,19 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
             BlinkButton(user2Squares[0].GetComponent<Button>(), Color.green);
         }
     }
-    private List<Button> CheckWinCondition()
+    private List<Button> CheckWinCondition(int player)
     {
         List<Button> winningButtons = new List<Button>();
         for (int i = 0; i < 3; i++)
         {
-            if (squareStatus[i, 0] == currentPlayer && squareStatus[i, 1] == currentPlayer && squareStatus[i, 2] == currentPlayer)
+            if (squareStatus[i, 0] == player && squareStatus[i, 1] == player && squareStatus[i, 2] == player)
             {
                 winningButtons.Add(buttons[i, 0]);
                 winningButtons.Add(buttons[i, 1]);
                 winningButtons.Add(buttons[i, 2]);
                 return winningButtons;
             }
-            if (squareStatus[0, i] == currentPlayer && squareStatus[1, i] == currentPlayer && squareStatus[2, i] == currentPlayer)
+            if (squareStatus[0, i] == player && squareStatus[1, i] == player && squareStatus[2, i] == player)
             {
                 winningButtons.Add(buttons[0, i]);
                 winningButtons.Add(buttons[1, i]);
@@ -195,14 +197,14 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
                 return winningButtons;
             }
         }
-        if (squareStatus[0, 0] == currentPlayer && squareStatus[1, 1] == currentPlayer && squareStatus[2, 2] == currentPlayer)
+        if (squareStatus[0, 0] == player && squareStatus[1, 1] == player && squareStatus[2, 2] == player)
         {
             winningButtons.Add(buttons[0, 0]);
             winningButtons.Add(buttons[1, 1]);
             winningButtons.Add(buttons[2, 2]);
             return winningButtons;
         }
-        if (squareStatus[0, 2] == currentPlayer && squareStatus[1, 1] == currentPlayer && squareStatus[2, 0] == currentPlayer)
+        if (squareStatus[0, 2] == player && squareStatus[1, 1] == player && squareStatus[2, 0] == player)
         {
             winningButtons.Add(buttons[0, 2]);
             winningButtons.Add(buttons[1, 1]);
@@ -213,7 +215,7 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
     }
     private void DisableAllButtons()
     {
-        foreach (Button button in buttons)
+        foreach (Button button in buttonList)
         {
             button.interactable = false;
         }
@@ -243,7 +245,7 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
             StopBlinking(btn, Color.green);
         }
     }
-    private void HighlightWinningButtons(List<Button> winningButtons)
+    private void HighlightWinningButtons(IEnumerable<Button> winningButtons)
     {
         foreach (Button btn in winningButtons)
         {
@@ -254,7 +256,7 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
     {
         if (stream.IsWriting)
         {
-            stream.SendNext(squareStatus);
+            stream.SendNext(FlattenArray(squareStatus));  // 1次元配列に変換して送信
             stream.SendNext(currentPlayer);
             stream.SendNext(turnCount);
             stream.SendNext(previousButtonX);
@@ -262,11 +264,41 @@ public class GameScript : MonoBehaviourPunCallbacks, IPunObservable
         }
         else
         {
-            squareStatus = (int[,])stream.ReceiveNext();
+            squareStatus = UnflattenArray((int[])stream.ReceiveNext(), 3, 3);  // 1次元配列から多次元配列に変換
             currentPlayer = (int)stream.ReceiveNext();
             turnCount = (int)stream.ReceiveNext();
             previousButtonX = (int)stream.ReceiveNext();
             previousButtonY = (int)stream.ReceiveNext();
         }
+        // currentPlayerが同期された際にターンの状態も更新する
+        UpdateTurnStatus();
+    }
+    // 多次元配列を1次元配列に変換
+    private int[] FlattenArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+        int[] flat = new int[rows * cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                flat[i * cols + j] = array[i, j];
+            }
+        }
+        return flat;
+    }
+    // 1次元配列を多次元配列に変換
+    private int[,] UnflattenArray(int[] array, int rows, int cols)
+    {
+        int[,] multi = new int[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                multi[i, j] = array[i * cols + j];
+            }
+        }
+        return multi;
     }
 }
